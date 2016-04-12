5c138a4c9438a360271cac732cd5e8e90b368dc7
Stefan Mehnert <stefanmehnert@gmail.com>
2016-04-12 20:54:59 +0200

    example 2: Subset of Instances

--- 04_yield_return/Program.cs
+++ 04_yield_return/Program.cs
@@ -9,8 +9,14 @@ namespace ConsoleApplication
         public static void Main(string[] args)
         {
             PowerOf2Example();
+            
+            InstanceSubsetExampe();
         }
         
+        
+        /*
+            Example 1: Power of 2
+        */
         private static void PowerOf2Example()
         {
             var numbers = new List<int>() {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
@@ -31,5 +37,48 @@ namespace ConsoleApplication
                 }
             }
         }  
+        
+        
+        /*
+            Example 2: Return a subset of objects using yield return
+        */
+        private static void InstanceSubsetExampe()
+        {
+            var persons = new List<Person>() { new Person(15, "Carl"), new Person(20, "Dan"), new Person(25, "Mitchell") };
+            
+            foreach (var adultPerson in GetAdults(persons))
+            {
+                Console.WriteLine(adultPerson);
+            }
+        }
+        
+        private static IEnumerable<Person> GetAdults(IEnumerable<Person> persons)
+        {
+            foreach (var person in persons)
+            {
+                if(person.Age >= 18)
+                {
+                    yield return person;
+                }
+            }
+        }
+        
+    }
+    
+    class Person
+    {
+        public int Age { get; }
+        public string Name { get; }
+        
+        public Person(int age, string name)
+        {
+            this.Age = age;
+            this.Name = name;
+        }
+        
+        public override string ToString()
+        {
+            return $"{ Name }: { Age }";
+        }
     }
 }
